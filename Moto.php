945b8c7223d25247c3fffd4c6e8990be56ae20c9
Fabian <fabianchock@gmail.com>
2024-04-12 12:42:54 -0300

    Funcion darPrecioVenta

--- Moto.php
+++ Moto.php
@@ -57,10 +57,32 @@ class Moto{
         $this->activa = $activa;
     }
     /* Metodo darPrecioVenta */
+    public function darPrecioVenta(){
+$precioVenta = 0;
+/* comprobamos que la moto se encuentre activa */
+if($this->getActiva()){
+/* Si la moto está disponible para la venta, el método realiza el siguiente cálculo: */
+$precioVenta = $this->getCosto() + $this->getCosto() * (($this->getAnioFabricacion() - date('Y') ) * $this->getPorcIncrementoAnual());
+}
+else{
+    /* si la moto no se encuentra activa seteamos el valor de precio a cero */
+    $precioVenta = -1;
+}
+return $precioVenta;
+
+    }
     /* Metodo ToString */
     public function __toString()
     {
-        return "\n Codigo moto: ".$this->getCodigo()."\n Costo:  ".$this->getCosto()." \n Año de Fabricacion: ".$this->getAnioFabricacion()." \n Descripcion: ".$this->getDescripcion()." \n Porcentaje De Incremento Anual: ".$this->getPorcIncrementoAnual()." \n Actividad para Venta: ".$this->getActiva()."\n ";
+        $actividad = "";
+        if($this->getActiva()){
+            $actividad = " Si ";
+        }
+        else{
+            $actividad= " No ";
+        }
+        return "\n Codigo moto: ".$this->getCodigo()."\n Costo:  ".$this->getCosto()." \n Año de Fabricacion: ".$this->getAnioFabricacion()." \n Descripcion: ".$this->getDescripcion()." \n Porcentaje De Incremento Anual: ".$this->getPorcIncrementoAnual()." \n Actividad para Venta: ".$actividad."\n ";
+    
     }
 
 }
