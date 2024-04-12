8d455063279981dd989be4f29a1eded15ae00192
Fabian <fabianchock@gmail.com>
2024-04-12 19:40:16 -0300

    Clase Empresa: Funcion retornar Moto

--- Empresa.php
+++ Empresa.php
@@ -50,6 +50,26 @@ class Empresa{
     public function setColVentasRealizadas($colVentasRealizadas){
         $this->colVentasRealizadas = $colVentasRealizadas;
     }
+/* METODOS ESPECIALES */
+/* Metodo RetornarMoto($codigoMoto) */
+/* Implementar el método retornarMoto($codigoMoto) que recorre la colección de motos de la Empresa y
+retorna la referencia al objeto moto cuyo código coincide con el recibido por parámetro. */
+public function retornarMoto($codigoMoto){
+     /* Recorremos el arreglo hasta encontrar el codigo que coincida (while) */
+     $buscando = true;
+     $i = 0;
+     $objMoto = null;
+     /* Se buscara mientras la variable bandera no marque haber encontrado algo y mientras no se agoten las posiciones del arreglo de motos */
+     while($buscando && $i < count($this->getColMotos())){
+     if($this->getColMotos()[$i]->getCodigo() == $codigoMoto){
+        $objMoto = $this->getColMotos()[$i];
+    $buscando = false;
+     }
+     $i++;
+}
+return $objMoto;
+}
+
     /* Metodo ToString */
     public function __toString()
     {
